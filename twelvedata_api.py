cf05d723fab8b21ce9a1f08e370c0131a4f5c2a9
FXjirka <jirisvihlik123@gmail.com>
2025-12-11 09:04:49 +0100

    Update twelvedata_api.py

--- twelvedata_api.py
+++ twelvedata_api.py
@@ -1,7 +1,7 @@
 import requests
 import pandas as pd
 
-API_KEY = "8f99b35c39634de89f340a1604b355f6"  # nahraď svým klíčem
+API_KEY = "8cb61bbdf66f84ba4952d645daec6d807"  # nahraď svým klíčem
 
 def get_ohlc(pair: str, interval: str, outputsize=100):
     """
